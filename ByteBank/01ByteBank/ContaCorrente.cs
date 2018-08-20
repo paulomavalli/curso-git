67048111eac269294a50828b3a909ec0acbbfb33
paulo.mavalli <paulo.mavalli@iteris.com.br>
2018-08-20 13:26:32 -0300

    training orientacao ao objeto conta corrente

---
+++ ByteBank/01ByteBank/ContaCorrente.cs
@@ -0,0 +1,19 @@
+﻿public class ContaCorrente
+{
+    public string titular;
+    public int ag;
+    public int conta;
+    public double saldo;
+
+    public bool Sacar( double valor )
+    {
+        if(this.saldo < valor)
+        {
+            return false;
+        }
+        else {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+}
---
+++ ByteBank/01ByteBank/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              ContaCorrente contaPaulo = new ContaCorrente();
+            contaPaulo.titular = "Paulo";
+            contaPaulo.ag = 1620;
+            contaPaulo.conta = 480230;
+            contaPaulo.saldo = 150.40;
+
+            Console.WriteLine(" Titular da conta : " + contaPaulo.titular);
+            Console.WriteLine(" Agencia : " + contaPaulo.ag);
+            Console.WriteLine(" Conta Corrente : " + contaPaulo.conta);
+            Console.WriteLine(" Saldo Disponivel : R$ " + contaPaulo.saldo);
+
+            bool resultadoSaque = contaPaulo.Sacar(50.20);
+
+            Console.WriteLine(" Saque efetuado");
+            Console.WriteLine( resultadoSaque);
+            Console.WriteLine(" Saldo Disponivel apos o saque : R$ " + contaPaulo.saldo);
+
+
+
+            Console.ReadLine();
+
+           
+
+        }
+    }
+}
