842c3e6b36df21486b04ba4f691d7188d6431fef
paulo.mavalli <paulo.mavalli@iteris.com.br>
2018-08-20 10:31:06 -0300

    training csharp lacos

---
+++ lacosEnc13.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //*
+            //**
+            //***
+            //****
+            //*****
+            //******
+            //*******
+            //********
+
+            for ( int contadorLinha=0; contadorLinha >= 10; contadorLinha++) 
+                {
+                    for( int contadorColuna =0; contadorColuna <= contadorLinha; contadorColuna++)
+                    {
+                    Console.Write("*");
+                    }
+                Console.WriteLine();
+               
+                }
+            Console.ReadLine();
+        }
+    }
+}
+    }
+}
